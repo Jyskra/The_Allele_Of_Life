fafc6ef25d06f1e291d08f4676e580eda59aa15c
jyskra <jira.baudys@gmail.com>
2026-05-22 15:01:52 +0200

    random push

---
+++ src/Simulation/MainMenu.java
@@ -0,0 +1,13 @@
+package Simulation;
+
+import Settings.Setting;
+
+import java.util.ArrayList;
+
+public class MainMenu {
+    private ArrayList<Setting> activeSettings = new ArrayList<>();
+
+    public MainMenu(){
+
+    }
+}
